26cf6cb66d100dc0ecef7c9d15673b1cb913b83c
Aqin23 <71952789+Aqin23@users.noreply.github.com>
2021-03-25 23:20:47 +0700

    pertemuan1

---
+++ app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controller;
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+   public function index(){
+       return "Hallo ini adalah method index, dalam controller ManagementUser.";
+   }
+}
